a0d150e3a7844be107146eb0519fce7cd6caaf38
seveibar <seveibar@gmail.com>
2024-08-13 07:59:15 -0700

    generate symbols

--- scripts/generate-symbols-from-asset-svgs.ts
+++ scripts/generate-symbols-from-asset-svgs.ts
@@ -27,98 +27,116 @@ async function processSvg() {
     }
 
     // Process each subgroup under the root
-    rootGroup.children.forEach((group, index) => {
-      if (group.name === "g") {
+    rootGroup.children
+      .filter((child) => child.name === "g")
+      .forEach((group, index) => {
         const groupId = group.attributes.id
-        const filePath = `./assets/symbols-svg-json/${groupId}.json`
-        let groupWithTransformApplied = applyGroupTransformsToChildren(group)
+        try {
+          const filePath = `./assets/symbols-svg-json/${groupId}.json`
+          let groupWithTransformApplied = applyGroupTransformsToChildren(group)
 
-        // Recenter the group (makes it easier to read)
-        let bounds = getBoundsOfSvgJson(groupWithTransformApplied as any)
-        groupWithTransformApplied.attributes.transform = toSVG(
-          compose(scale(0.1, 0.1), translate(-bounds.centerX, -bounds.centerY)),
-        )
-        groupWithTransformApplied = applyGroupTransformsToChildren(
-          groupWithTransformApplied,
-        )
+          // Recenter the group (makes it easier to read)
+          let bounds = getBoundsOfSvgJson(groupWithTransformApplied as any)
+          groupWithTransformApplied.attributes.transform = toSVG(
+            compose(
+              scale(0.1, 0.1),
+              translate(-bounds.centerX, -bounds.centerY),
+            ),
+          )
+          groupWithTransformApplied = applyGroupTransformsToChildren(
+            groupWithTransformApplied,
+          )
 
-        bounds = getBoundsOfSvgJson(groupWithTransformApplied as any)
+          bounds = getBoundsOfSvgJson(groupWithTransformApplied as any)
 
-        const refblocks = convertToObjectWithOrderedPositionIds(
-          groupWithTransformApplied.children
-            .filter(
-              (child) =>
-                child.name === "circle" &&
-                (child.attributes["inkscape:label"]?.includes("refblock") ||
-                  child.attributes.id?.includes("refblock")),
-            )
-            .map((child) => ({
-              x: parseFloat(child.attributes.cx),
-              y: parseFloat(child.attributes.cy),
+          const refblocks = convertToObjectWithOrderedPositionIds(
+            groupWithTransformApplied.children
+              .filter(
+                (child) =>
+                  child.name === "circle" &&
+                  (child.attributes["inkscape:label"]?.includes("refblock") ||
+                    child.attributes.id?.includes("refblock")),
+              )
+              .map((child) => ({
+                x: parseFloat(child.attributes.cx),
+                y: parseFloat(child.attributes.cy),
+              })),
+          )
+
+          const textChildren = groupWithTransformApplied.children.filter(
+            (text) => text.name === "text",
+          )
+          const texts = convertToObjectWithOrderedPositionIds(
+            textChildren.map((text) => ({
+              type: "text",
+              text: findInnerText(text),
+              x: parseFloat(text.attributes.x),
+              y: parseFloat(text.attributes.y),
             })),
-        )
+          )
 
-        const textChildren = groupWithTransformApplied.children.filter(
-          (text) => text.name === "text",
-        )
-        const texts = convertToObjectWithOrderedPositionIds(
-          textChildren.map((text) => ({
-            type: "text",
-            text: findInnerText(text),
-            x: parseFloat(text.attributes.x),
-            y: parseFloat(text.attributes.y),
-          })),
-        )
+          const pathChildren = groupWithTransformApplied.children.filter(
+            (path) => path.name === "path",
+          )
+          const paths = Object.fromEntries(
+            pathChildren.map((path) => [
+              path.attributes.id!,
+              {
+                type: "path",
+                points: svgPathToPoints(path.attributes.d!),
+                color: "primary",
+                fill:
+                  path.attributes.fill === "true" &&
+                  !path.attributes.style?.includes("fill:none"),
+              },
+            ]),
+          )
 
-        const pathChildren = groupWithTransformApplied.children.filter(
-          (path) => path.name === "path",
-        )
-        const paths = Object.fromEntries(
-          pathChildren.map((path) => [
-            path.attributes.id!,
-            {
-              type: "path",
-              points: svgPathToPoints(path.attributes.d!),
-              color: "primary",
-              fill:
-                path.attributes.fill === "true" &&
-                !path.attributes.style?.includes("fill:none"),
-            },
-          ]),
-        )
+          const svgData = {
+            // only for debugging
+            // svg: groupWithTransformApplied,
+            paths,
+            texts,
+            refblocks,
+            bounds,
+          }
 
-        const svgData = {
-          // only for debugging
-          // svg: groupWithTransformApplied,
-          paths,
-          texts,
-          refblocks,
-          bounds,
+          console.log(`Writing to file: ${filePath}`)
+          fs.writeFileSync(filePath, JSON.stringify(svgData, null, 2))
+
+          if (SOURCE_IGNORE_LIST.includes(groupId)) {
+            return
+          }
+
+          // Check if there's a source file for this symbol
+          const outputPath = `./symbols/${groupId}_horz.ts`
+          const hasSourceFile = fs.existsSync(outputPath)
+          const isReadyForGen =
+            Object.keys(texts).length > 0 &&
+            Object.keys(paths).length > 0 &&
+            Object.keys(refblocks).length > 0
+
+          if (!hasSourceFile && !isReadyForGen) {
+            console.log(
+              `Skipping ${groupId} because it's not ready for generation (missing some elements)`,
+            )
+            console.log(`  - texts: ${Object.keys(texts).length}`)
+            console.log(`  - paths: ${Object.keys(paths).length}`)
+            console.log(`  - refblocks: ${Object.keys(refblocks).length}`)
+          } else if (!hasSourceFile && isReadyForGen) {
+            console.log(`Creating horz source file: ${outputPath}`)
+            const content = getTsFileContentForSvgGroup(groupId, svgData as any)
+            fs.writeFileSync(outputPath, content)
+            // Write the vert file
+            const vertOutputPath = `./symbols/${groupId}_vert.ts`
+            console.log(`Creating vert source file: ${vertOutputPath}`)
+            const contentVert = `import { rotateSymbol } from "drawing/rotateSymbol"\nimport ${groupId}_horz from "./${groupId}_horz"\n\nexport default rotateSymbol(${groupId}_horz)`
+            fs.writeFileSync(vertOutputPath, contentVert)
+          }
+        } catch (err: any) {
+          console.log(`Error processing ${groupId}: ${err.message}`)
         }
-
-        console.log(`Writing to file: ${filePath}`)
-        fs.writeFileSync(filePath, JSON.stringify(svgData, null, 2))
-
-        if (SOURCE_IGNORE_LIST.includes(groupId)) {
-          return
-        }
-
-        // Check if there's a source file for this symbol
-        const outputPath = `./symbols/${groupId}_horz.ts`
-        const hasSourceFile = fs.existsSync(outputPath)
-
-        if (!hasSourceFile) {
-          console.log(`Creating horz source file: ${outputPath}`)
-          const content = getTsFileContentForSvgGroup(groupId, svgData as any)
-          fs.writeFileSync(outputPath, content)
-          // Write the vert file
-          const vertOutputPath = `./symbols/${groupId}_vert.ts`
-          console.log(`Creating vert source file: ${vertOutputPath}`)
-          const contentVert = `import { rotateSymbol } from "drawing/rotateSymbol"\nimport ${groupId}_horz from "./${groupId}_horz"\n\nexport default rotateSymbol(${groupId}_horz)`
-          fs.writeFileSync(vertOutputPath, contentVert)
-        }
-      }
-    })
+      })
   } catch (error) {
     console.error("Error processing SVG:", error)
   }
