0c5d183500f414a4481ff4b749d9ccd360e3002d
seveibar <seveibar@gmail.com>
2024-08-17 18:26:27 -0700

    include some computed fields to make it easier to use symbols

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { default as symbols } from "./symbols"
+import symbols from "./symbols"
 export type {
   SchSymbol,
   BoxPrimitive,
@@ -6,3 +6,17 @@ export type {
   PathPrimitive,
   TextPrimitive,
 } from "./drawing/types"
+
+export type BaseSymbolName =
+  keyof typeof symbols extends `${infer T}_${infer U}` ? `${T}` : never
+
+/**
+ * Utility for easier autocomplete:
+ *
+ * BASE_SYMBOLS.boxresistor
+ * // "boxresistor"
+ */
+export const BASE_SYMBOLS: Record<BaseSymbolName, BaseSymbolName> =
+  Object.fromEntries(Object.keys(symbols).map((k) => [k, k])) as any
+
+export { symbols }
