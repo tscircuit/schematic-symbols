ae655a1b0ca8dba61da4952043f7c995289a9f87
seveibar <seveibar@gmail.com>
2024-08-20 14:49:57 -0700

    minor modification to packagejson for types gen

--- symbols/index.ts
+++ symbols/index.ts
@@ -14,18 +14,18 @@ import _varistor_horz from "./varistor_horz"
 import _varistor_vert from "./varistor_vert"
 
 export default {
-  boxresistor_horz: _boxresistor_horz,
-  boxresistor_vert: _boxresistor_vert,
-  fuse_horz: _fuse_horz,
-  fuse_vert: _fuse_vert,
-  led_horz: _led_horz,
-  led_vert: _led_vert,
-  mosfet_depletion_normally_on_horz: _mosfet_depletion_normally_on_horz,
-  mosfet_depletion_normally_on_vert: _mosfet_depletion_normally_on_vert,
-  potentiometer_horz: _potentiometer_horz,
-  potentiometer_vert: _potentiometer_vert,
-  potentiometer2_horz: _potentiometer2_horz,
-  potentiometer2_vert: _potentiometer2_vert,
-  varistor_horz: _varistor_horz,
-  varistor_vert: _varistor_vert,
+  "boxresistor_horz": _boxresistor_horz,
+  "boxresistor_vert": _boxresistor_vert,
+  "fuse_horz": _fuse_horz,
+  "fuse_vert": _fuse_vert,
+  "led_horz": _led_horz,
+  "led_vert": _led_vert,
+  "mosfet_depletion_normally_on_horz": _mosfet_depletion_normally_on_horz,
+  "mosfet_depletion_normally_on_vert": _mosfet_depletion_normally_on_vert,
+  "potentiometer_horz": _potentiometer_horz,
+  "potentiometer_vert": _potentiometer_vert,
+  "potentiometer2_horz": _potentiometer2_horz,
+  "potentiometer2_vert": _potentiometer2_vert,
+  "varistor_horz": _varistor_horz,
+  "varistor_vert": _varistor_vert
 }
