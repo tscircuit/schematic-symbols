577861a7c63229d5a2ac5f0ceee658690e7f817c
Rishabh Gupta <belikerishabh@gmail.com>
2024-09-26 17:40:46 +0530

    Ref and value text added

--- symbols/capacitor_horz.ts
+++ symbols/capacitor_horz.ts
@@ -1,10 +1,14 @@
 import svgJson from "assets/symbols-svg-json/capacitor.json"
 import { defineSymbol } from "drawing/defineSymbol"
 
-const { paths, bounds } = svgJson
+const { paths, bounds, refblocks, texts } = svgJson
 
 export default defineSymbol({
-  primitives: [...Object.values(paths)] as any,
+  primitives: [
+    ...Object.values(paths),
+    { ...texts.top1, anchor: "center" },
+    { ...texts.bottom1, anchor: "center" },
+  ] as any,
   ports: [],
   size: { width: bounds.width, height: bounds.height }, //{ width: 1, height: 0.24 },
   center: { x: bounds.centerX, y: bounds.centerY },
