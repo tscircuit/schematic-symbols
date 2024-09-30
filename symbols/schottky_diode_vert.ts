c443cbb5f76687f97a18c12a619d0c642ca8c963
Anas sarkiz <anassoftwaredev@gmail.com>
2024-09-30 20:22:39 +0200

    formatted

--- symbols/schottky_diode_vert.ts
+++ symbols/schottky_diode_vert.ts
@@ -6,10 +6,10 @@ import type { TextPrimitive } from "drawing"
 const rotated = rotateSymbol(schottky_diode_horz)
 
 const ref = rotated.primitives.find(
-    (p) => p.type === "text" && p.text === "{REF}",
+  (p) => p.type === "text" && p.text === "{REF}",
 )! as TextPrimitive
 const val = rotated.primitives.find(
-    (p) => p.type === "text" && p.text === "{VAL}",
+  (p) => p.type === "text" && p.text === "{VAL}",
 )! as TextPrimitive
 
 ref.anchor = "middle_left"
