86cbc1a57f153a842b99fc0079e6c90f23c51c65
Anas sarkiz <anassoftwaredev@gmail.com>
2024-10-01 10:23:04 +0200

    Formatted

--- symbols/avalanche_diode_vert.ts
+++ symbols/avalanche_diode_vert.ts
@@ -5,10 +5,10 @@ import type { TextPrimitive } from "drawing"
 
 const rotated = rotateSymbol(avalanche_diode_horz)
 const ref = rotated.primitives.find(
-    (p) => p.type === "text" && p.text === "{REF}",
+  (p) => p.type === "text" && p.text === "{REF}",
 )! as TextPrimitive
 const val = rotated.primitives.find(
-    (p) => p.type === "text" && p.text === "{VAL}",
+  (p) => p.type === "text" && p.text === "{VAL}",
 )! as TextPrimitive
 
 ref.anchor = "middle_left"
