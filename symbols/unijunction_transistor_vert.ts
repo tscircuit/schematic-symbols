8ce953c10d7009196be6dd5490c9c652342018a2
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-10-03 02:34:36 -0400

    fix: the placement of text ref/val

---
+++ symbols/unijunction_transistor_vert.ts
@@ -0,0 +1,17 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import unijunction_transistor_horz from "./unijunction_transistor_horz"
+
+const rotatedSymbol = rotateSymbol(unijunction_transistor_horz)
+
+const texts = rotatedSymbol.primitives.filter(primitive => primitive.type === "text")
+
+const ref = texts.find(text => text.text === "{REF}")!
+
+ref.y = 0.1
+
+const val = texts.find(text => text.text === "{VAL}")!
+
+val.y = 0.1
+val.x = -0.4
+
+export default rotatedSymbol
