ee406f1263ca9945870108a061aa351a13e6ed6e
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-10-03 02:37:11 -0400

    style: fix the forgotten format run

--- symbols/unijunction_transistor_vert.ts
+++ symbols/unijunction_transistor_vert.ts
@@ -3,13 +3,15 @@ import unijunction_transistor_horz from "./unijunction_transistor_horz"
 
 const rotatedSymbol = rotateSymbol(unijunction_transistor_horz)
 
-const texts = rotatedSymbol.primitives.filter(primitive => primitive.type === "text")
+const texts = rotatedSymbol.primitives.filter(
+  (primitive) => primitive.type === "text",
+)
 
-const ref = texts.find(text => text.text === "{REF}")!
+const ref = texts.find((text) => text.text === "{REF}")!
 
 ref.y = 0.1
 
-const val = texts.find(text => text.text === "{VAL}")!
+const val = texts.find((text) => text.text === "{VAL}")!
 
 val.y = 0.1
 val.x = -0.4
