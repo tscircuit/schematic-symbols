0b0b3600c7ddd49e310d307e1b1d997322b438e1
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-10-03 02:34:12 -0400

    feat: created svg and ts for unijunction_transistor

--- generated/symbols-index.ts
+++ generated/symbols-index.ts
@@ -38,6 +38,8 @@ import _triac_horz from "./../symbols/triac_horz"
 import _triac_vert from "./../symbols/triac_vert"
 import _tunnel_diode_horz from "./../symbols/tunnel_diode_horz"
 import _tunnel_diode_vert from "./../symbols/tunnel_diode_vert"
+import _unijunction_transistor_horz from "./../symbols/unijunction_transistor_horz"
+import _unijunction_transistor_vert from "./../symbols/unijunction_transistor_vert"
 import _varactor_diode_horz from "./../symbols/varactor_diode_horz"
 import _varactor_diode_vert from "./../symbols/varactor_diode_vert"
 import _varistor_horz from "./../symbols/varistor_horz"
@@ -88,6 +90,8 @@ export default {
   "triac_vert": _triac_vert,
   "tunnel_diode_horz": _tunnel_diode_horz,
   "tunnel_diode_vert": _tunnel_diode_vert,
+  "unijunction_transistor_horz": _unijunction_transistor_horz,
+  "unijunction_transistor_vert": _unijunction_transistor_vert,
   "varactor_diode_horz": _varactor_diode_horz,
   "varactor_diode_vert": _varactor_diode_vert,
   "varistor_horz": _varistor_horz,
