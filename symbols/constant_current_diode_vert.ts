58074b9982c308e054a26485680cd80d88043ac2
Aman SINGH <rockaman155@gmail.com>
2024-10-05 03:55:37 +0530

    type_check

--- symbols/constant_current_diode_vert.ts
+++ symbols/constant_current_diode_vert.ts
@@ -3,15 +3,15 @@ import constant_current_diode_horz from "./constant_current_diode_horz"
 
 const rotatedSymbol = rotateSymbol(constant_current_diode_horz)
 
-const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")
+const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")!
 
-const val = texts.find((t) => t.text === "{VAL}")
+const val = texts.find((t) => t.text === "{VAL}")!
 
 val.x = -0.35
 val.y = 0
 val.anchor = "middle_right"
 
-const ref = texts.find((t) => t.text === "{REF}")
+const ref = texts.find((t) => t.text === "{REF}")!
 
 ref.y = 0
 ref.x = 0.35
