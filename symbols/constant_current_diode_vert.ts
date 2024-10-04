7fda6f472b4d2a61bf71194d96d47de76b72ecb5
Aman SINGH <rockaman155@gmail.com>
2024-10-05 03:33:31 +0530

    center ref and val

--- symbols/constant_current_diode_vert.ts
+++ symbols/constant_current_diode_vert.ts
@@ -8,11 +8,13 @@ const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")
 const val = texts.find((t) => t.text === "{VAL}")
 
 val.x = -0.35
+val.y=0
 val.anchor = "middle_right"
 
 const ref = texts.find((t) => t.text === "{REF}")
 
 ref.y = 0
+ref.x = 0.35
 ref.anchor = "middle_left"
 
 export default rotatedSymbol
