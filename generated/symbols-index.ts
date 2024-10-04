421d91de6e9b3a1943d0c0c4350992781b9f64db
Aman SINGH <rockaman155@gmail.com>
2024-10-05 03:06:39 +0530

    COnstant current diode

--- generated/symbols-index.ts
+++ generated/symbols-index.ts
@@ -6,6 +6,8 @@ import _boxresistor_horz from "./../symbols/boxresistor_horz"
 import _boxresistor_vert from "./../symbols/boxresistor_vert"
 import _capacitor_horz from "./../symbols/capacitor_horz"
 import _capacitor_vert from "./../symbols/capacitor_vert"
+import _constant_current_diode_horz from "./../symbols/constant_current_diode_horz"
+import _constant_current_diode_vert from "./../symbols/constant_current_diode_vert"
 import _dc_ammeter_horz from "./../symbols/dc_ammeter_horz"
 import _dc_ammeter_vert from "./../symbols/dc_ammeter_vert"
 import _dc_voltmeter_horz from "./../symbols/dc_voltmeter_horz"
@@ -62,6 +64,8 @@ export default {
   "boxresistor_vert": _boxresistor_vert,
   "capacitor_horz": _capacitor_horz,
   "capacitor_vert": _capacitor_vert,
+  "constant_current_diode_horz": _constant_current_diode_horz,
+  "constant_current_diode_vert": _constant_current_diode_vert,
   "dc_ammeter_horz": _dc_ammeter_horz,
   "dc_ammeter_vert": _dc_ammeter_vert,
   "dc_voltmeter_horz": _dc_voltmeter_horz,
---
+++ symbols/constant_current_diode_vert.ts
@@ -0,0 +1,18 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import constant_current_diode_horz from "./constant_current_diode_horz"
+
+const rotatedSymbol = rotateSymbol(constant_current_diode_horz)
+
+const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")
+
+const val = texts.find((t) => t.text === "{VAL}")
+
+val.x=-0.35
+val.anchor = "middle_right"
+
+const ref = texts.find((t)=>t.text==="{REF}")
+
+ref.y=0
+ref.anchor = "middle_left"
+
+export default rotatedSymbol
