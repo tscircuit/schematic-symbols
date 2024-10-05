90326747d2150c1784cdf72a580775eddf8e3f71
bhavyajain511 <bjbhavyajain2004@gmail.com>
2024-10-05 20:52:57 +0530

    use bezier curve for ohm

--- symbols/ohm_meter_vert.ts
+++ symbols/ohm_meter_vert.ts
@@ -1,19 +1,4 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import ohm_meter_horz from "./ohm_meter_horz"
-import { Primitive } from "drawing/types"
 
-const { 2: ohm, ...rest } = ohm_meter_horz.primitives
-
-function isPrimitive(value: any): value is Primitive {
-  return typeof value === "object"
-}
-
-const rotatedSymbol = rotateSymbol({
-  ...ohm_meter_horz,
-  primitives: Object.values(rest).filter(isPrimitive),
-})
-
-export default {
-  ...rotatedSymbol,
-  primitives: [...rotatedSymbol.primitives, ohm],
-}
+export default rotateSymbol(ohm_meter_horz)
