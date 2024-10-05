a244e23d5f43464903c1cfab717492c951afa725
Manoprasanth MATHAVAN <manoprasanth.mathavan@gmail.com>
2024-10-06 00:46:45 +0530

    add resonator

--- generated/symbols-index.ts
+++ generated/symbols-index.ts
@@ -40,6 +40,8 @@ import _push_button_normally_closed_momentary_horz from "./../symbols/push_butto
 import _push_button_normally_closed_momentary_vert from "./../symbols/push_button_normally_closed_momentary_vert"
 import _push_button_normally_open_momentary_horz from "./../symbols/push_button_normally_open_momentary_horz"
 import _push_button_normally_open_momentary_vert from "./../symbols/push_button_normally_open_momentary_vert"
+import _resonator_horz from "./../symbols/resonator_horz"
+import _resonator_vert from "./../symbols/resonator_vert"
 import _schottky_diode_horz from "./../symbols/schottky_diode_horz"
 import _schottky_diode_vert from "./../symbols/schottky_diode_vert"
 import _silicon_controlled_rectifier_horz from "./../symbols/silicon_controlled_rectifier_horz"
@@ -104,6 +106,8 @@ export default {
   "push_button_normally_closed_momentary_vert": _push_button_normally_closed_momentary_vert,
   "push_button_normally_open_momentary_horz": _push_button_normally_open_momentary_horz,
   "push_button_normally_open_momentary_vert": _push_button_normally_open_momentary_vert,
+  "resonator_horz": _resonator_horz,
+  "resonator_vert": _resonator_vert,
   "schottky_diode_horz": _schottky_diode_horz,
   "schottky_diode_vert": _schottky_diode_vert,
   "silicon_controlled_rectifier_horz": _silicon_controlled_rectifier_horz,
