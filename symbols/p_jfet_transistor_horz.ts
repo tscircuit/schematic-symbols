f7efa56c9981ad5a43d467304c46c278c1b0e059
Aman SINGH <rockaman155@gmail.com>
2024-10-06 05:11:58 +0530

    format

--- symbols/p_jfet_transistor_horz.ts
+++ symbols/p_jfet_transistor_horz.ts
@@ -8,12 +8,12 @@ export default defineSymbol({
     ...Object.values(paths),
     ...Object.values(circles),
     { ...texts.top1, anchor: "middle_center" },
-    { ...texts.bottom1, anchor: "middle_center", x:-0.15 },
+    { ...texts.bottom1, anchor: "middle_center", x: -0.15 },
   ] as any,
   ports: [
     { ...refblocks.top1, labels: ["1", "drain"] }, // TODO add more "standard" labels
     { ...refblocks.bottom1, labels: ["2", "source"] }, // TODO add more "standard" labels
-    { ...refblocks.left1, labels: ["3", "gate"]  }, // TODO add more "standard" labels
+    { ...refblocks.left1, labels: ["3", "gate"] }, // TODO add more "standard" labels
   ],
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
