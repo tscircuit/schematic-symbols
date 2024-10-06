7ea5863a051383557f12001ef300297fb0255730
Aman SINGH <rockaman155@gmail.com>
2024-10-07 00:43:13 +0530

    format

--- symbols/n_channel_d_mosfet_transistor_horz.ts
+++ symbols/n_channel_d_mosfet_transistor_horz.ts
@@ -7,13 +7,13 @@ export default defineSymbol({
   primitives: [
     ...Object.values(paths),
     ...Object.values(circles),
-    { ...texts.top1, anchor: "middle_right", x:0 },
-    { ...texts.bottom1, anchor: "middle_right", x:0 },
+    { ...texts.top1, anchor: "middle_right", x: 0 },
+    { ...texts.bottom1, anchor: "middle_right", x: 0 },
   ] as any,
   ports: [
-    { ...refblocks.top1, labels: ["1","drain"] }, // TODO add more "standard" labels
-    { ...refblocks.bottom1, labels: ["2","source"] }, // TODO add more "standard" labels
-    { ...refblocks.left1, labels: ["3","gate"] }, // TODO add more "standard" labels
+    { ...refblocks.top1, labels: ["1", "drain"] }, // TODO add more "standard" labels
+    { ...refblocks.bottom1, labels: ["2", "source"] }, // TODO add more "standard" labels
+    { ...refblocks.left1, labels: ["3", "gate"] }, // TODO add more "standard" labels
   ],
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
