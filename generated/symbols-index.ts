cfdda9369b7535c076e09d0d55a077f87b371cd6
Aman SINGH <rockaman155@gmail.com>
2024-10-11 00:43:10 +0530

    SPST switch

--- generated/symbols-index.ts
+++ generated/symbols-index.ts
@@ -58,6 +58,8 @@ import _schottky_diode_horz from "./../symbols/schottky_diode_horz"
 import _schottky_diode_vert from "./../symbols/schottky_diode_vert"
 import _silicon_controlled_rectifier_horz from "./../symbols/silicon_controlled_rectifier_horz"
 import _silicon_controlled_rectifier_vert from "./../symbols/silicon_controlled_rectifier_vert"
+import _SPST_switch_horz from "./../symbols/SPST_switch_horz"
+import _SPST_switch_vert from "./../symbols/SPST_switch_vert"
 import _step_recovery_diode_horz from "./../symbols/step_recovery_diode_horz"
 import _step_recovery_diode_vert from "./../symbols/step_recovery_diode_vert"
 import _triac_horz from "./../symbols/triac_horz"
@@ -138,6 +140,8 @@ export default {
   "schottky_diode_vert": _schottky_diode_vert,
   "silicon_controlled_rectifier_horz": _silicon_controlled_rectifier_horz,
   "silicon_controlled_rectifier_vert": _silicon_controlled_rectifier_vert,
+  "SPST_switch_horz": _SPST_switch_horz,
+  "SPST_switch_vert": _SPST_switch_vert,
   "step_recovery_diode_horz": _step_recovery_diode_horz,
   "step_recovery_diode_vert": _step_recovery_diode_vert,
   "triac_horz": _triac_horz,
