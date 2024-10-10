4edf174551a65e8c251ee3347f75a6a2febc8e7e
0SlowPoke0 <govindpratyush@gmail.com>
2024-10-11 00:09:14 +0530

    fixes_pn_juction_diode

--- symbols/pn_junction_diode_horz.ts
+++ symbols/pn_junction_diode_horz.ts
@@ -1,8 +1,6 @@
 import { defineSymbol } from "drawing/defineSymbol"
 import svgJson from "assets/generated/pn_junction_diode.json"
-import { Primitive } from "drawing/types"
 import { TextPrimitive } from "drawing/types"
-
 
 const { paths, texts, bounds, refblocks, circles } = svgJson
 
