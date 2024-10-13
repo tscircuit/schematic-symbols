e600fcdcb891b479cf16692b1f07916bbe81ac7e
Aman SINGH <rockaman155@gmail.com>
2024-10-14 00:35:46 +0530

    update horz

--- symbols/dpst_switch_horz.ts
+++ symbols/dpst_switch_horz.ts
@@ -12,10 +12,10 @@ export default defineSymbol({
     { ...texts.bottom1, anchor: "middle_top", x: 0 },
   ] as Primitive[],
   ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.left3, labels: ["3"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-    { ...refblocks.right3, labels: ["4"] }, // TODO add more "standard" labels
+    { ...refblocks.left1, labels: ["1", "left1"] }, // TODO add more "standard" labels
+    { ...refblocks.left3, labels: ["3", "left3"] }, // TODO add more "standard" labels
+    { ...refblocks.right1, labels: ["2", "right1"] }, // TODO add more "standard" labels
+    { ...refblocks.right3, labels: ["4", "right3"] }, // TODO add more "standard" labels
   ],
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
