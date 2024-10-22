8caf949ade656bfb94e9e246cce3361579c840a2
TSP06 <tanusiwach2005@gmail.com>
2024-10-22 07:37:37 +0530

    added grnd symbol

---
+++ symbols/ground_horz.ts
@@ -0,0 +1,4 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import ground_vert from "./ground_vert.ts"
+
+export default rotateSymbol(ground_vert)
---
+++ symbols/ground_vert.ts
@@ -0,0 +1,37 @@
+import { defineSymbol } from "drawing/defineSymbol"
+import svgJson from "assets/generated/ground.json"
+import { Primitive } from "drawing/types"
+
+const { paths, texts, bounds, refblocks, circles } = svgJson
+
+const adjustedPort1Position = {
+  right1: { x: 5, y: 10 },
+  right2: { x: 4, y: 7 },
+}
+
+export default defineSymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0.3,
+      y: -0.325,
+      anchor: "middle_bottom",
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.3,
+      y: 0.325,
+      anchor: "middle_top",
+    },
+  ] as Primitive[],
+  ports: [
+    { ...refblocks.right1, labels: ["1"] }, // TODO add more "standard" labels
+    { ...refblocks.right2, labels: ["2"] }, // TODO add more "standard" labels
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX + 0.5, y: bounds.centerY + 0.1 },
+})
