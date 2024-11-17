9e57b8eed6dbe048e2f8be47f463f1fdc6cae10e
seveibar <seveibar@gmail.com>
2024-11-17 12:59:14 -0800

    fix weird diode paths

--- symbols/diode_right.ts
+++ symbols/diode_right.ts
@@ -1,20 +1,10 @@
 import { defineSymbol } from "drawing/defineSymbol"
 import svgJson from "assets/generated/diode.json"
-import { Primitive } from "drawing/types"
+import { modifySymbol } from "scripts/lib/modify-symbol/modify-symbol"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
-
-export default defineSymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    { ...texts.top1, anchor: "middle_bottom" },
-    { ...texts.bottom1, anchor: "middle_top" },
-  ] as Primitive[],
-  ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-  ],
-  size: { width: bounds.width, height: bounds.height },
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
+export default modifySymbol(svgJson)
+  .labelPort("left1", ["1", "pos"])
+  .labelPort("right1", ["2", "neg"])
+  .changeTextAnchor("{REF}", "middle_bottom")
+  .changeTextAnchor("{VAL}", "middle_top")
+  .build()
