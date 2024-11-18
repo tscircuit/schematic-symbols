ef4484c4b1f46d7066a02d1ecfa7739968645941
seveibar <seveibar@gmail.com>
2024-11-18 11:22:23 -0800

    move modifySymbol to drawing directory for export

--- symbols/ground_vert.ts
+++ symbols/ground_vert.ts
@@ -2,6 +2,6 @@ import { defineSymbol } from "drawing/defineSymbol"
 import svgJson from "assets/generated/ground.json"
 import { Primitive } from "drawing/types"
 import groundHorz from "./ground_horz"
-import { modifySymbol } from "scripts/lib/modify-symbol/modify-symbol"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
 export default modifySymbol(groundHorz).rotateRightFacingSymbol("down").build()
