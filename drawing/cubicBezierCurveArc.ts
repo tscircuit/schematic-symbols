a41afd7cf80964c333329e316bca4b4f2a2a776a
AbseLY <abdaslam13.2.2001@gmail.com>
2024-11-19 18:54:46 +0200

    Create approximateBezier function for improved 'C' case handling and add new symbol capacitor_polarised

---
+++ drawing/cubicBezierCurveArc.ts
@@ -0,0 +1,28 @@
+import type { Point } from "./types"
+
+export function approximateBezier(
+  p0: Point,
+  p1: Point,
+  p2: Point,
+  p3: Point,
+): Point[] {
+  const points: Point[] = []
+  const steps = 100
+  for (let t = 0; t <= 1; t += 1 / steps) {
+    const x =
+      Math.pow(1 - t, 3) * p0.x +
+      3 * Math.pow(1 - t, 2) * t * p1.x +
+      3 * (1 - t) * Math.pow(t, 2) * p2.x +
+      Math.pow(t, 3) * p3.x
+
+    const y =
+      Math.pow(1 - t, 3) * p0.y +
+      3 * Math.pow(1 - t, 2) * t * p1.y +
+      3 * (1 - t) * Math.pow(t, 2) * p2.y +
+      Math.pow(t, 3) * p3.y
+
+    points.push({ x, y })
+  }
+
+  return points
+}
---
+++ symbols/capacitor_polarized_down.ts
@@ -0,0 +1,4 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import capacitor_polarized_right from "./capacitor_polarized_right"
+
+export default rotateSymbol(capacitor_polarized_right, "down")
