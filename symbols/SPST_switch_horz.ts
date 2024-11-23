518c47498987984c9e3a2b8e6e5ec149144f599c
hunxjunedo <junaidhunain6@gmail.com>
2024-11-23 15:22:31 +0500

    fix type issue

--- symbols/SPST_switch_horz.ts
+++ symbols/SPST_switch_horz.ts
@@ -1,12 +1,9 @@
-import { defineSymbol } from "drawing/defineSymbol"
 import svgJson from "assets/generated/SPST_switch.json"
-import { Primitive } from "drawing/types"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-delete svgJson.refblocks.left1
-delete svgJson.refblocks.right1
+delete (svgJson.refblocks as any).left1
+delete (svgJson.refblocks as any).right1
 svgJson.bounds.width += 0.2
-const { paths, texts, bounds, refblocks, circles } = svgJson
 export default modifySymbol(svgJson)
   .changeTextAnchor("{VAL}", "middle_top")
   .labelPort("left2", ["1"])
