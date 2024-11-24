84aaa7bc62496e1e40579ea14123fbd2f80782f1
ni9999 <nazmulislamzrn@yahoo.com>
2024-11-24 07:36:31 +0600

    fix labelport

--- symbols/crystal_4pin_horz.ts
+++ symbols/crystal_4pin_horz.ts
@@ -2,8 +2,8 @@ import svgJson from "assets/generated/crystal_4pin.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
 export default modifySymbol(svgJson)
-  .labelPort("top1", ["gnd1"])
-  .labelPort("bottom1", ["gnd2"])
+  .labelPort("top1", ["2", "gnd1"])
+  .labelPort("bottom1", ["4", "gnd2"])
   .labelPort("left1", ["1"])
   .labelPort("right1", ["3"])
   .changeTextAnchor("{REF}", "bottom_right")
