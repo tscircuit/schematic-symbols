3dc23cdb68eec2dc21db6458c8336a6fd1bb8418
AbseLY <abdaslam13.2.2001@gmail.com>
2024-11-26 09:35:51 +0200

    improved the normally open and closed push buttons symbols

--- symbols/push_button_normally_open_momentary_vert.ts
+++ symbols/push_button_normally_open_momentary_vert.ts
@@ -1,23 +1,4 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import push_button_normally_open_momentary_horz from "./push_button_normally_open_momentary_horz"
-import type { TextPrimitive } from "drawing"
 
-const rotated = rotateSymbol(push_button_normally_open_momentary_horz)
-
-const ref = rotated.primitives.find(
-  (p) => p.type === "text" && p.text === "{REF}",
-)! as TextPrimitive
-const val = rotated.primitives.find(
-  (p) => p.type === "text" && p.text === "{VAL}",
-)! as TextPrimitive
-
-ref.anchor = "middle_left"
-val.anchor = "middle_right"
-
-ref.x = ref.x + 0.1
-ref.y = ref.y
-
-val.x = val.x
-val.y = 0
-
-export default rotated
+export default rotateSymbol(push_button_normally_open_momentary_horz, "up")
