3636bf6f583446aa3bc978ad7f7784d9bd8ddde4
AbseLY <abdaslam13.2.2001@gmail.com>
2024-12-05 16:48:04 +0200

    adjusted resistor anchors and made them consistent when flipping

--- symbols/boxresistor_down.ts
+++ symbols/boxresistor_down.ts
@@ -1,9 +1,33 @@
 import svgJson from "assets/generated/boxresistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default modifySymbol(svgJson)
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.16,
+      y: 0.2294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.16,
+      y: 0.2294553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
+    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .changeTextAnchor("{VAL}", "middle_bottom")
   .rotateRightFacingSymbol("down")
-  .changeTextAnchor("{VAL}", "middle_right")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
   .changeTextAnchor("{REF}", "middle_left")
--- symbols/boxresistor_up.ts
+++ symbols/boxresistor_up.ts
@@ -1,10 +1,34 @@
 import svgJson from "assets/generated/boxresistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("up")
-  .changeTextAnchor("{VAL}", "middle_left")
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.16,
+      y: 0.2294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.16,
+      y: 0.2294553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
+    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .changeTextAnchor("{VAL}", "middle_bottom")
+  .rotateRightFacingSymbol("down")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_right")
+  .changeTextAnchor("{REF}", "middle_left")
   .build()
