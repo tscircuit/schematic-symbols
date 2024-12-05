be494b32f986c4e2abda910b0150e5e01f75f85a
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2024-12-05 09:40:18 +0200

    adjusted REF & VAL for capacitors (#219)

--- symbols/capacitor_left.ts
+++ symbols/capacitor_left.ts
@@ -1,4 +1,11 @@
-import { rotateSymbol } from "drawing/rotateSymbol"
-import capacitor_horz from "./capacitor_horz"
+import { isPrimitive } from "drawing/typeguards"
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
+import svgJson from "./capacitor_right"
 
-export default rotateSymbol(capacitor_horz, "left")
+export default modifySymbol(svgJson)
+  .changeTextAnchor("{VAL}", "middle_top")
+  .rotateRightFacingSymbol("left")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_top")
+  .build()
--- symbols/capacitor_right.ts
+++ symbols/capacitor_right.ts
@@ -1,3 +1,10 @@
-import capacitor_horz from "./capacitor_horz"
+import svgJson from "assets/generated/capacitor.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default capacitor_horz
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("right")
+  .changeTextAnchor("{VAL}", "middle_right")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .build()
