ee292f6bdbd09254b335ce5fc60ba8ddc08b4f14
techmannih <manishchaudharyttt@gmail.com>
2024-12-20 13:57:56 +0530

    add dpdp_switch

---
+++ symbols/dpdp_switch_vert.ts
@@ -0,0 +1,20 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import dpdp_switch_horz from "./dpdp_switch_horz"
+
+const rotatedSymbol = rotateSymbol(dpdp_switch_horz)
+
+const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")!
+
+const val = texts.find((t) => t.text === "{VAL}")!
+
+val.anchor = "middle_right"
+val.x = -0.37
+val.y = 0.12
+
+const ref = texts.find((t) => t.text === "{REF}")!
+
+ref.anchor = "middle_left"
+ref.x = 0.37
+ref.y = 0.12
+
+export default rotatedSymbol
