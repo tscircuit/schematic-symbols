35591b8040f39354c4c0c17d918c04709d0893c3
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-01-14 08:24:40 +0530

    fix ref text for capacitor left (#234)

--- symbols/capacitor_left.ts
+++ symbols/capacitor_left.ts
@@ -1,11 +1,11 @@
 import { isPrimitive } from "drawing/typeguards"
 import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
-import svgJson from "./capacitor_right"
+import svgJson from "../assets/generated/capacitor.json"
 
 export default modifySymbol(svgJson)
   .changeTextAnchor("{VAL}", "middle_top")
-  .rotateRightFacingSymbol("left")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
+  .rotateRightFacingSymbol("right")
+  .labelPort("left1", ["2"])
+  .labelPort("right1", ["1"])
+  .changeTextAnchor("{REF}", "middle_bottom")
   .build()
