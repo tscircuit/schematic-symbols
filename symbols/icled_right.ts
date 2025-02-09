5045705c3ce139f3c09c54085e4d30a1a1da4df7
techmannih <manishchaudharyttt@gmail.com>
2025-02-09 23:50:33 +0530

    replace ref and val

--- symbols/icled_right.ts
+++ symbols/icled_right.ts
@@ -2,10 +2,10 @@ import svgJson from "assets/generated/icled.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
 export default modifySymbol(svgJson)
-  .labelPort("left1", ["1", "power"])
-  .labelPort("left2", ["2", "dout"])
-  .labelPort("right1", ["3", "din"])
-  .labelPort("right2", ["4", "gnd"])
+  .labelPort("left1", ["1", "DIN"])
+  .labelPort("left2", ["2", "POWER"])
+  .labelPort("right1", ["4", "GND"])
+  .labelPort("right2", ["3", "DOUT"])
   .changeTextAnchor("{REF}", "middle_bottom")
   .changeTextAnchor("{VAL}", "middle_top")
   .build()
