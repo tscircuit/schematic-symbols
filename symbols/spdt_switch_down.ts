5b037aab9a53b9b01498da0ad4b20ceaafb99640
techmannih <manishchaudharyttt@gmail.com>
2025-02-18 02:12:57 +0530

    add all orientation for spdt and spst

---
+++ symbols/spdt_switch_down.ts
@@ -0,0 +1,22 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import spdt_switch_right from "./spdt_switch_right"
+import type { TextPrimitive } from "drawing"
+
+const rotated = rotateSymbol(spdt_switch_right, "down")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+const val = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{VAL}",
+)! as TextPrimitive
+
+ref.anchor = "middle_bottom"
+val.anchor = "middle_top"
+
+ref.x = 0.44
+ref.y += 0.28
+val.y -= 0.28
+val.x = 0.44
+
+export default rotated
