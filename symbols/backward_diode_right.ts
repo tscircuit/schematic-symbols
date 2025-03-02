5ec4496dd9f6102b55708810fe88a5b5cb22178c
Saurabhsing21 <saurabhsingh881888@gmail.com>
2025-03-02 16:35:19 +0530

    fixed symmetric and  text alignment

--- symbols/backward_diode_right.ts
+++ symbols/backward_diode_right.ts
@@ -1,29 +1,29 @@
 import { defineSymbol } from "drawing/defineSymbol"
 import svgJson from "assets/generated/backward_diode.json"
 import { Primitive } from "drawing/types"
-import { TextPrimitive } from "dist"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
 const { paths, texts, bounds, refblocks, circles } = svgJson
 
-export default defineSymbol({
+export default modifySymbol({
   primitives: [
     ...Object.values(paths),
     ...Object.values(circles),
     {
       type: "text",
       text: "{REF}",
-      x: 0,
-      y: -0.2,
-      anchor: "middle_top",
-    } as TextPrimitive,
+      x: -0.0050250999999996715,
+      y: 0.27384529999999985,
+      anchor: "middle_left",
+    },
     {
       type: "text",
       text: "{VAL}",
-      x: 0,
-      y: 0.2,
-      anchor: "middle_bottom",
-    } as TextPrimitive,
-  ] as Primitive[],
+      x: -0.00011179999999999524,
+      y: -0.27384530000000007,
+      anchor: "middle_left",
+    },
+  ] as any[],
   ports: [
     { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
     { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
@@ -31,3 +31,9 @@ export default defineSymbol({
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
 })
+  .changeTextAnchor("{VAL}", "middle_top")
+  .rotateRightFacingSymbol("right")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_bottom")
+  .build()
