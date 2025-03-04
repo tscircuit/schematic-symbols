c8b9d2d601a817577c934067fcd27e8cb2db7050
Saurabhsing21 <saurabhsingh881888@gmail.com>
2025-03-04 09:11:37 +0530

    fixed backwoard up

--- symbols/backward_diode_up.ts
+++ symbols/backward_diode_up.ts
@@ -1,36 +1,29 @@
 import svgJson from "assets/generated/backward_diode.json"
-import { Primitive } from "drawing/types"
+import { defineSymbol } from "drawing/defineSymbol"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
+import { rotateSymbol } from "drawing/rotateSymbol"
 
-export default modifySymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    {
-      type: "text",
-      text: "{REF}",
-      x: -0.16,
-      y: 0.2294553499999995,
-    },
-    {
-      type: "text",
-      text: "{VAL}",
-      x: 0.16,
-      y: 0.2294553499999995,
-    },
-  ] as any,
-  ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-  ],
-  size: { width: bounds.width, height: bounds.height },
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
-  .changeTextAnchor("{VAL}", "middle_bottom")
-  .rotateRightFacingSymbol("down")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_left")
-  .build()
+import type { TextPrimitive } from "drawing"
+import tilted_digital_ground_right from "./backward_diode_left"
+import backward_diode_left from "./backward_diode_left"
+
+const { paths, texts, bounds, refblocks, circles } = svgJson
+const rotated = rotateSymbol(backward_diode_left, "down")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+const val = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{VAL}",
+)! as TextPrimitive
+
+ref.anchor = "middle_left"
+val.anchor = "middle_left"
+
+ref.x = 0.25
+ref.y += 0.21
+val.y -= 0.21
+val.x = 0.25
+
+export default rotated
