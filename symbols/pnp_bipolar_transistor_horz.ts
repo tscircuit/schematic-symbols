858f4566c06b39057b250c601bf34599b6013969
techmannih <manishchaudharyttt@gmail.com>
2025-04-14 00:15:55 +0530

    fix: correct pnp transistor

--- symbols/pnp_bipolar_transistor_horz.ts
+++ symbols/pnp_bipolar_transistor_horz.ts
@@ -1,10 +1,13 @@
 import svgJson from "assets/generated/pnp_bipolar_transistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { flipSymbolOverXAxis } from "drawing/rotateSymbol"
 
-export default modifySymbol(svgJson)
-  .changeTextAnchor("{VAL}", "middle_right")
+const pnp_bipolar_transistor_horz = modifySymbol(svgJson)
   .labelPort("left1", ["3"])
   .labelPort("bottom1", ["2"])
   .labelPort("top1", ["1"])
-  .changeTextAnchor("{REF}", "middle_right")
+  .changeTextAnchor("{VAL}", "middle_right")
+  .changeTextAnchor("{REF}", "middle_left")
   .build()
+
+export default flipSymbolOverXAxis(pnp_bipolar_transistor_horz)
