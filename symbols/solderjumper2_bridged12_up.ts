3d00b3856f2b79a57201ce1de1bc56ef093a9541
Shibo <shibosoftwaredev@gmail.com>
2025-05-08 18:20:56 +0200

    solderjumper2_bridged12

---
+++ symbols/solderjumper2_bridged12_up.ts
@@ -0,0 +1,4 @@
+import { rotateSymbol } from "drawing/rotateSymbol"
+import solderjumper2_bridged12_right from "./solderjumper2_bridged12_right"
+
+export default rotateSymbol(solderjumper2_bridged12_right, "up")
