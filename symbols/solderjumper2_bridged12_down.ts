48889908e50eb32f54683199978650b3e675b745
Shibo <shibosoftwaredev@gmail.com>
2025-05-08 20:45:17 +0200

    polished

--- symbols/solderjumper2_bridged12_down.ts
+++ symbols/solderjumper2_bridged12_down.ts
@@ -1,4 +1,9 @@
-import { rotateSymbol } from "drawing/rotateSymbol"
-import solderjumper2_bridged12_right from "./solderjumper2_bridged12_right"
+import svgJson from "../assets/generated/solderjumper2_bridged12.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default rotateSymbol(solderjumper2_bridged12_right, "down")
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("down")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .build()
--- symbols/solderjumper2_bridged12_right.ts
+++ symbols/solderjumper2_bridged12_right.ts
@@ -1,19 +1,9 @@
-import { defineSymbol } from "drawing/defineSymbol"
-import svgJson from "assets/generated/solderjumper2_bridged12.json"
-import { Primitive } from "drawing/types"
+import svgJson from "../assets/generated/solderjumper2_bridged12.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
-
-export default defineSymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    { ...texts.top1, anchor: "middle_left" },
-  ] as Primitive[],
-  ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-  ],
-  size: { width: bounds.width, height: bounds.height },
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("right")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_bottom")
+  .build()
