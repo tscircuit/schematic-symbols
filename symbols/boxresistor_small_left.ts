3b392d530f6efdd225db3e6d95ca8bb94b0c7bc4
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-05-15 20:07:17 +0200

    new schematic symbol boxresistor_small (#276)

---
+++ symbols/boxresistor_small_left.ts
@@ -0,0 +1,34 @@
+import svgJson from "../assets/generated/boxresistor_small.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0,
+      y: -0.2294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0,
+      y: 0.1704553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
+    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .rotateRightFacingSymbol("left")
+  .labelPort("left", ["1"])
+  .labelPort("right", ["2"])
+  .changeTextAnchor("{REF}", "middle_bottom")
+  .changeTextAnchor("{VAL}", "middle_top")
+  .build()
