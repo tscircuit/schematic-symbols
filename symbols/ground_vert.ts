eb25c6a61860ecb1ee27c708170a8d9ef1bc01fb
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-05-18 21:14:11 +0530

    fix ground symbol (#281)

--- symbols/ground_vert.ts
+++ symbols/ground_vert.ts
@@ -1,4 +1,22 @@
-import groundHorz from "./ground_horz"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { rotateSymbol } from "drawing/rotateSymbol"
+import ground_horz from "./ground_horz"
+import type { TextPrimitive } from "drawing"
 
-export default modifySymbol(groundHorz).rotateRightFacingSymbol("down").build()
+const symbol = rotateSymbol(ground_horz, "down")
+
+const ref = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+const val = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{VAL}",
+)! as TextPrimitive
+
+ref.anchor = "middle_left"
+val.anchor = "middle_left"
+
+ref.y += 0.16
+ref.x = 0.31
+val.y -= 0.25
+val.x = 0.31
+
+export default symbol
