431375b527c78411f8790abc72e1149639d4e0fb
Shibo <shibosoftwaredev@gmail.com>
2025-06-03 19:26:20 +0200

    Revert "smaller vcc symbol" (#290)
    This reverts commit 7a0855e21f765ad8c94eee3751a7814088bd2c1f.

--- symbols/vcc_up.ts
+++
@@ -1,8 +0,0 @@
-import svgJson from "../assets/generated/vcc.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
-
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("up")
-  .labelPort("left1", ["1"])
-  .changeTextAnchor("{REF}", "middle_bottom")
-  .build()
