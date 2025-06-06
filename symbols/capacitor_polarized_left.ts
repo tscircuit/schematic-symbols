e346161996294c52e71f2621ce8c305540826a39
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-06 12:47:40 +0530

    fixed REF and VAL text position for polarized capacitor left symbol (#297)

--- symbols/capacitor_polarized_left.ts
+++ symbols/capacitor_polarized_left.ts
@@ -1,10 +1,34 @@
-import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
-import svgJson from "./capacitor_polarized_right"
+import svgJson from "assets/generated/capacitor_polarized.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default modifySymbol(svgJson)
-  .changeTextAnchor("{VAL}", "middle_top")
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0,
+      y: -0.4094553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: -0,
+      y: 0.4094553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
+    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .changeTextAnchor("{VAL}", "middle_bottom")
   .rotateRightFacingSymbol("left")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
+  .changeTextAnchor("{REF}", "middle_bottom")
   .build()
