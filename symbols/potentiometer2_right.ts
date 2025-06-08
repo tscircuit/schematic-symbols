2c5eec0452c02bc805a1faf70b6e7d8f3ebd0ff9
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-08 13:54:49 +0530

    add potentiometer2 symbol for all variation (#300)

--- symbols/potentiometer2_right.ts
+++ symbols/potentiometer2_right.ts
@@ -6,13 +6,13 @@ const { paths, texts, bounds, refblocks } = svgJson
 export default defineSymbol({
   primitives: [
     ...Object.values(paths),
-    { ...texts.bottom1, anchor: "middle_top" },
-    { ...texts.top1, anchor: "middle_bottom" },
+    { ...texts.bottom1, x: 0, y: -0.35, anchor: "middle_top" },
+    { ...texts.top1, x: 0, y: 0.35, anchor: "middle_bottom" },
   ] as any,
   ports: [
     { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
     { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
   ],
-  size: { width: bounds.width, height: bounds.height },
+  size: { width: bounds.width + 0.05, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
 })
