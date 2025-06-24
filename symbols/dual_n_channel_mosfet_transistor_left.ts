d50bb4c0dc3d303fbad86af6dc65ada82403b500
techmannih <manishchaudharyttt@gmail.com>
2025-06-24 22:33:23 +0530

    update left snapshot

--- symbols/dual_n_channel_mosfet_transistor_left.ts
+++ symbols/dual_n_channel_mosfet_transistor_left.ts
@@ -1,26 +1,4 @@
-import svgJson from "assets/generated/dual_n_channel_mosfet_transistor.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { flipSymbolOverYAxis } from "drawing/rotateSymbol"
+import dual_n_channel_mosfet_transistor_right from "./dual_n_channel_mosfet_transistor_right"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
-export default modifySymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    {
-      type: "text",
-      text: "{REF}",
-      x: 0,
-      y: 0.2594553499999995,
-    },
-  ] as any,
-  ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-  ],
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
-  .rotateRightFacingSymbol("left")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
-  .build()
+export default flipSymbolOverYAxis(dual_n_channel_mosfet_transistor_right)
