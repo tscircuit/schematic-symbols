a856c54e80f8c4781d240c71395998b2dff536ed
techmannih <manishchaudharyttt@gmail.com>
2025-06-24 10:48:22 +0530

    add ref text

--- symbols/dual_n_channel_mosfet_transistor_down.ts
+++ symbols/dual_n_channel_mosfet_transistor_down.ts
@@ -1,9 +1,18 @@
 import svgJson from "assets/generated/dual_n_channel_mosfet_transistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-const { paths, bounds, refblocks, circles } = svgJson
+const { paths, texts, bounds, refblocks, circles } = svgJson
 export default modifySymbol({
-  primitives: [...Object.values(paths), ...Object.values(circles)] as any,
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0,
+      y: 0.2594553499999995,
+    },
+  ] as any,
   ports: [
     { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
     { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
--- symbols/dual_n_channel_mosfet_transistor_left.ts
+++ symbols/dual_n_channel_mosfet_transistor_left.ts
@@ -1,18 +1,26 @@
 import svgJson from "assets/generated/dual_n_channel_mosfet_transistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-const { paths, bounds, refblocks, circles } = svgJson
+const { paths, texts, bounds, refblocks, circles } = svgJson
 export default modifySymbol({
-  primitives: [...Object.values(paths), ...Object.values(circles)] as any,
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0,
+      y: 0.2594553499999995,
+    },
+  ] as any,
   ports: [
     { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
     { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
   ],
   center: { x: bounds.centerX, y: bounds.centerY },
 })
-  .changeTextAnchor("{VAL}", "middle_bottom")
   .rotateRightFacingSymbol("left")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_bottom")
+  .changeTextAnchor("{REF}", "middle_top")
   .build()
