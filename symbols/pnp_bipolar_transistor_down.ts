07225243ea042c1e407e1f08e87c3f887f321f58
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-07-11 16:11:49 +0200

    patch

--- symbols/pnp_bipolar_transistor_down.ts
+++ symbols/pnp_bipolar_transistor_down.ts
@@ -1,10 +1,33 @@
 import svgJson from "../assets/generated/pnp_bipolar_transistor.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default modifySymbol(svgJson)
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.1,
+      y: 0.3094553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: -0.1,
+      y: -0.3094553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.top1, labels: ["1"] },
+    { ...refblocks.bottom1, labels: ["2"] },
+    { ...refblocks.left1, labels: ["3"] },
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
   .rotateRightFacingSymbol("down")
-  .labelPort("top1", ["1"])
-  .labelPort("bottom1", ["2"])
-  .labelPort("left1", ["3"])
   .changeTextAnchor("{REF}", "middle_left")
+  .changeTextAnchor("{VAL}", "middle_right")
   .build()
