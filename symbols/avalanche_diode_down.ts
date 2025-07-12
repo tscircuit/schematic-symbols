c773aa2d50be91b85ac144beb2be74e52e0e0768
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-07-12 16:51:05 +0200

    patch

--- symbols/avalanche_diode_down.ts
+++ symbols/avalanche_diode_down.ts
@@ -1,10 +1,34 @@
 import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/avalanche_diode.json"
 
-export default modifySymbol(svgJson)
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.15,
+      y: 0.3294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.15,
+      y: 0.3294553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] },
+    { ...refblocks.right1, labels: ["2"] },
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
   .rotateRightFacingSymbol("down")
-  .labelPort("left1", ["1", "neg"])
-  .labelPort("right1", ["2", "pos"])
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
   .changeTextAnchor("{REF}", "middle_left")
-  .changeTextAnchor("{VAL}", "middle_right")
+  .changeTextAnchor("{VAL}", "middle_left")
   .build()
--- symbols/avalanche_diode_vert.ts
+++ symbols/avalanche_diode_vert.ts
@@ -1,10 +1,34 @@
 import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/avalanche_diode.json"
 
-export default modifySymbol(svgJson)
+const { paths, texts, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.15,
+      y: -0.2294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.15,
+      y: -0.2294553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] },
+    { ...refblocks.right1, labels: ["2"] },
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
   .rotateRightFacingSymbol("up")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
   .changeTextAnchor("{VAL}", "middle_left")
-  .labelPort("left1", ["1", "neg"])
-  .labelPort("right1", ["2", "pos"])
-  .changeTextAnchor("{REF}", "middle_right")
   .build()
