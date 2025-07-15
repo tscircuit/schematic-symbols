be155b784c379336c63d2d7d1ddf4ac26b1ab83d
techmannih <manishchaudharyttt@gmail.com>
2025-07-15 12:01:51 +0530

    fix REF and VAl anchor test for potentiometer3

--- symbols/potentiometer3_up.ts
+++ symbols/potentiometer3_up.ts
@@ -1,4 +1,21 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import potentiometer3_right from "./potentiometer3_right"
+import type { TextPrimitive } from "drawing"
 
-export default rotateSymbol(potentiometer3_right, "up")
+const symbol = rotateSymbol(potentiometer3_right, "up")
+
+const ref = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+const val = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{VAL}",
+)! as TextPrimitive
+
+ref.anchor = "middle_bottom"
+val.anchor = "middle_top"
+ref.y += 0
+ref.x = 0.35
+val.y -= 0.6
+val.x = 0.35
+
+export default symbol
