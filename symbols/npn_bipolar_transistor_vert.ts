3c11a3007d89f24c67058b0724e33004cd1e1322
techmannih <manishchaudharyttt@gmail.com>
2025-07-16 23:10:58 +0530

    Add Alternative Pin Labels (Base, Emitter, Collector) for NPN and PNP Bipolar Transistors

--- symbols/npn_bipolar_transistor_vert.ts
+++ symbols/npn_bipolar_transistor_vert.ts
@@ -19,9 +19,9 @@ export default modifySymbol({
     },
   ] as any,
   ports: [
-    { ...refblocks.top1, labels: ["1"] },
-    { ...refblocks.bottom1, labels: ["2"] },
-    { ...refblocks.left1, labels: ["3"] },
+    { ...refblocks.top1, labels: ["1", "collector"] },
+    { ...refblocks.bottom1, labels: ["2", "emitter"] },
+    { ...refblocks.left1, labels: ["3", "base"] },
   ],
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
