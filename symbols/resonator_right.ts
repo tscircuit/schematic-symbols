08bdf3922c4255ea8e2cfb6ed79f241d9f0f4127
techmannih <manishchaudharyttt@gmail.com>
2025-08-12 22:19:56 +0530

    fix

--- symbols/resonator_right.ts
+++ symbols/resonator_right.ts
@@ -7,22 +7,8 @@ export default defineSymbol({
   primitives: [
     ...Object.values(paths),
     ...Object.values(circles),
-    // { ...texts.top1, anchor: "middle_left" },
-    // { ...texts.bottom1, anchor: "middle_left" },
-    {
-      type: "text",
-      text: "{REF}",
-      x: 0.01596175000000022,
-      y: -0.5308501500000009,
-      anchor: "middle_top",
-    },
-    {
-      type: "text",
-      text: "{VAL}",
-      x: 0.013116750000000454,
-      y: 0.5408501499999989,
-      anchor: "middle_bottom",
-    },
+    { ...texts.top1, anchor: "middle_bottom" },
+    { ...texts.bottom1, anchor: "middle_top" },
   ] as any,
   ports: [
     { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
--- symbols/resonator_up.ts
+++ symbols/resonator_up.ts
@@ -1,4 +1,20 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import resonator_right from "./resonator_right"
+import type { TextPrimitive } from "drawing"
 
-export default rotateSymbol(resonator_right, "up")
+const symbol = rotateSymbol(resonator_right, "up")
+const ref = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+const val = symbol.primitives.find(
+  (p) => p.type === "text" && p.text === "{VAL}",
+)! as TextPrimitive
+
+ref.anchor = "middle_bottom"
+val.anchor = "middle_top"
+
+ref.y += 0.499
+ref.x = 0.62
+val.y -= 0.29999
+val.x += 0.1
+export default symbol
