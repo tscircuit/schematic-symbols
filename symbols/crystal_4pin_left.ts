f4bbd2dc6bda1d7c233b5c19f861839006d9b711
techmannih <manishchaudharyttt@gmail.com>
2025-08-14 21:20:05 +0530

    Fix Pin Numbering to Follow Counter Clockwise Convention For Crystal

--- symbols/crystal_4pin_left.ts
+++ symbols/crystal_4pin_left.ts
@@ -2,8 +2,8 @@ import svgJson from "assets/generated/crystal_4pin.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
 export default modifySymbol(svgJson)
-  .labelPort("top1", ["4", "gnd1"])
-  .labelPort("bottom1", ["2", "gnd2"])
+  .labelPort("top1", ["2", "gnd1"])
+  .labelPort("bottom1", ["4", "gnd2"])
   .labelPort("left1", ["3"])
   .labelPort("right1", ["1"])
   .changeTextAnchor("{REF}", "bottom_left")
