7727ffb12f15ee54182cc4cb42f25eea761a710e
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-08-18 20:54:19 +0530

    Fix Pin Numbering to Follow Counter Clockwise Convention For Darlington Pair Transistor (#346)
    * fix darlington pair pin numbering
    
    * Swap pin 2 and 3 for horizontal and right Darlington

--- symbols/darlington_pair_transistor_right.ts
+++ symbols/darlington_pair_transistor_right.ts
@@ -3,8 +3,8 @@ import svgJson from "assets/generated/darlington_pair_transistor.json"
 
 export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("right")
-  .labelPort("right1", ["1"])
-  .labelPort("right2", ["2"])
+  .labelPort("right2", ["1"])
+  .labelPort("right1", ["2"])
   .labelPort("right3", ["3"])
   .changeTextAnchor("{VAL}", "middle_right")
   .changeTextAnchor("{REF}", "middle_right")
