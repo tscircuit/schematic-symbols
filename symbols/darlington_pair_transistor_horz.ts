3ff62a80050f4573e6a958a28efee462eb27e9bf
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-08-18 17:39:00 +0530

    Fix Darlington pair transistor pin order

--- symbols/darlington_pair_transistor_horz.ts
+++ symbols/darlington_pair_transistor_horz.ts
@@ -3,9 +3,9 @@ import svgJson from "assets/generated/darlington_pair_transistor.json"
 
 export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("right")
-  .labelPort("right1", ["1"])
-  .labelPort("right2", ["2"])
-  .labelPort("right3", ["3"])
+  .labelPort("right2", ["1"])
+  .labelPort("right3", ["2"])
+  .labelPort("right1", ["3"])
   .changeTextAnchor("{VAL}", "middle_right")
   .changeTextAnchor("{REF}", "middle_right")
   .build()
