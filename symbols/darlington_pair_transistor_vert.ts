8b191f925a38abd33d76d6c5c6cce25f745c881e
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-08-18 17:34:38 +0530

    fix darlington pair pin numbering

--- symbols/darlington_pair_transistor_vert.ts
+++ symbols/darlington_pair_transistor_vert.ts
@@ -3,8 +3,8 @@ import svgJson from "assets/generated/darlington_pair_transistor.json"
 
 export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("up")
-  .labelPort("right1", ["1"])
-  .labelPort("right2", ["2"])
+  .labelPort("right2", ["1"])
+  .labelPort("right1", ["2"])
   .labelPort("right3", ["3"])
   .changeTextAnchor("{VAL}", "middle_left", { x: 0, y: -0.15 })
   .changeTextAnchor("{REF}", "middle_left", { x: 0.8, y: 0.15 })
