18214c5b61b29ae0e6a47f607ea605d25e1211bf
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-08-26 15:57:11 +0200

    new symbol: square wave

---
+++ symbols/square_wave_down.ts
@@ -0,0 +1,10 @@
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
+import svgJson from "assets/generated/square_wave.json"
+
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("down")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .changeTextAnchor("{VAL}", "middle_left")
+  .build()
