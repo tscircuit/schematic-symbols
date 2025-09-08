1d7f1a200a65d11f7a1bcc1827a0b2a4be4ad61b
Severin Ibarluzea <seve700@gmail.com>
2025-09-08 16:43:57 -0700

    Resize vcc symbol

--- symbols/vcc_up.ts
+++ symbols/vcc_up.ts
@@ -1,8 +1,11 @@
 import svgJson from "../assets/generated/vcc.json"
 import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { resizeSymbol } from "drawing/resizeSymbol"
 
-export default modifySymbol(svgJson)
+const symbol = modifySymbol(svgJson)
   .rotateRightFacingSymbol("up")
   .labelPort("left1", ["1"])
   .changeTextAnchor("{REF}", "middle_bottom")
   .build()
+
+export default resizeSymbol(symbol, { width: 0.24, height: 0.23 })
