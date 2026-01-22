8f41609438515584518a891ce83232015d50d94b
nailoo <rahulsinghnailoo@gmail.com>
2026-01-22 16:43:42 +0530

    introduce all orientation for push_button_normally_open_momentary

--- symbols/push_button_normally_open_momentary_left.ts
+++ symbols/push_button_normally_open_momentary_left.ts
@@ -1,4 +1,4 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import push_button_normally_open_momentary_right from "./push_button_normally_open_momentary_right"
 
-export default rotateSymbol(push_button_normally_open_momentary_right, "up")
+export default rotateSymbol(push_button_normally_open_momentary_right, "left")
