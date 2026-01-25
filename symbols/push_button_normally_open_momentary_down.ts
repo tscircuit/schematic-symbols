3b52a8dfe3accd8cbad64af80fd11b609ef13f7c
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2026-01-25 19:54:37 +0530

    introduce all orientation for push_button_normally_open_momentary (#376)
    * introduce all orientation for push_button_normally_open_momentary
    
    * improve ref and val text

--- symbols/push_button_normally_open_momentary_down.ts
+++ symbols/push_button_normally_open_momentary_down.ts
@@ -2,8 +2,7 @@ import { rotateSymbol } from "drawing/rotateSymbol"
 import push_button_normally_open_momentary_right from "./push_button_normally_open_momentary_right"
 import type { TextPrimitive } from "drawing"
 
-const symbol = rotateSymbol(push_button_normally_open_momentary_right, "up")
-
+const symbol = rotateSymbol(push_button_normally_open_momentary_right, "down")
 const ref = symbol.primitives.find(
   (p) => p.type === "text" && p.text === "{REF}",
 )! as TextPrimitive
@@ -11,12 +10,12 @@ const val = symbol.primitives.find(
   (p) => p.type === "text" && p.text === "{VAL}",
 )! as TextPrimitive
 
-ref.anchor = "middle_left"
-val.anchor = "middle_left"
+ref.anchor = "middle_bottom"
+val.anchor = "middle_top"
 
-ref.y += 0.4
-ref.x = 0.34
-val.y -= 0.41
-val.x = 0.34
+ref.y = 0.4
+ref.x = 0.35
+val.y = -0.4
+val.x = 0.35
 
 export default symbol
--- symbols/push_button_normally_open_momentary_left.ts
+++ symbols/push_button_normally_open_momentary_left.ts
@@ -1,8 +1,9 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import push_button_normally_open_momentary_right from "./push_button_normally_open_momentary_right"
+
 import type { TextPrimitive } from "drawing"
 
-const symbol = rotateSymbol(push_button_normally_open_momentary_right, "up")
+const symbol = rotateSymbol(push_button_normally_open_momentary_right, "left")
 
 const ref = symbol.primitives.find(
   (p) => p.type === "text" && p.text === "{REF}",
@@ -11,12 +12,12 @@ const val = symbol.primitives.find(
   (p) => p.type === "text" && p.text === "{VAL}",
 )! as TextPrimitive
 
-ref.anchor = "middle_left"
-val.anchor = "middle_left"
+ref.anchor = "middle_bottom"
+val.anchor = "middle_top"
 
-ref.y += 0.4
-ref.x = 0.34
-val.y -= 0.41
-val.x = 0.34
+ref.y = 0.4
+ref.x = 0
+val.y = -0.4
+val.x = 0
 
 export default symbol
