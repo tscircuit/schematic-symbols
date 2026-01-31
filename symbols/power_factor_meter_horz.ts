b5878358ade4168a74987d3c040107cd9f7c34b8
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2026-01-31 20:34:46 +0100

    fix

--- symbols/power_factor_meter_horz.ts
+++ symbols/power_factor_meter_horz.ts
@@ -1,42 +1,11 @@
-import { defineSymbol } from "drawing/defineSymbol"
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/power_factor_meter.json"
-import { ninePointAnchorToSvgAnchor } from "drawing/ninePointAnchorToSvgAnchor"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
-
-export default defineSymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    // { ...texts.top1, anchor: "middle_left" },
-    {
-      type: "text",
-      text: "{REF}",
-      x: -0.15,
-      y: 0.42330070000000064,
-      anchor: "middle_bottom",
-    },
-    // { ...texts.bottom1, anchor: "middle_left" },
-    {
-      type: "text",
-      text: "{VAL}",
-      x: -0.15,
-      y: -0.42330070000000064,
-      anchor: "middle_top",
-    },
-    // { ...texts.left1, anchor: "middle_left" },
-    {
-      type: "text",
-      text: "COS φ",
-      x: 0,
-      y: 0.014279000000000375,
-      anchor: "center",
-    },
-  ] as any,
-  ports: [
-    { ...refblocks.left1, labels: ["1"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["2"] }, // TODO add more "standard" labels
-  ],
-  size: { width: bounds.width, height: bounds.height },
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("right")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .changeTextAnchor("{VAL}", "middle_left")
+  .changeTextAnchor("COS φ", "middle_left")
+  .build()
--- symbols/power_factor_meter_vert.ts
+++ symbols/power_factor_meter_vert.ts
@@ -1,19 +1,11 @@
-import { rotateSymbol } from "drawing/rotateSymbol"
-import power_factor_meter_horz from "./power_factor_meter_horz"
-import { text } from "drawing/text"
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
+import svgJson from "assets/generated/power_factor_meter.json"
 
-const rotatedSymbol = rotateSymbol(power_factor_meter_horz)
-const texts = rotatedSymbol.primitives.filter((p) => p.type === "text")
-const ref = texts.find((t) => t.text === "{REF}")!
-const val = texts.find((t) => t.text === "{VAL}")!
-const text_cos = texts.find((t) => t.text === "COS φ")!
-ref.x = 0.35
-ref.y = 0
-ref.anchor = "middle_left"
-val.anchor = "middle_right"
-val.x = -0.35
-val.y = 0
-text_cos.x = 0
-text_cos.y = 0
-text_cos.anchor = "center"
-export default rotatedSymbol
+export default modifySymbol(svgJson)
+  .rotateRightFacingSymbol("up")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .changeTextAnchor("{VAL}", "middle_left")
+  .changeTextAnchor("COS φ", "middle_left")
+  .build()
