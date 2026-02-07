7801e7f59c7de4a3157bfaab66b85efec8923866
Shehab Ahmed <shehab924617940@gmail.com>
2026-02-07 19:19:40 +0200

    edit the '+' symbol

--- symbols/capacitor_polarized_right.ts
+++ symbols/capacitor_polarized_right.ts
@@ -1,32 +1,10 @@
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/capacitor_polarized.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-const { paths, texts, bounds, refblocks, circles } = svgJson
-export default modifySymbol({
-  primitives: [
-    ...Object.values(paths),
-    ...Object.values(circles),
-    {
-      type: "text",
-      text: "{REF}",
-      x: 0,
-      y: -0.4094553499999995,
-    },
-    {
-      type: "text",
-      text: "{VAL}",
-      x: -0,
-      y: 0.4094553499999995,
-    },
-  ] as any,
-  ports: [
-    { ...refblocks.left1, labels: ["2", "neg"] }, // TODO add more "standard" labels
-    { ...refblocks.right1, labels: ["1", "pos"] }, // TODO add more "standard" labels
-  ],
-  size: { width: bounds.width, height: bounds.height },
-  center: { x: bounds.centerX, y: bounds.centerY },
-})
-  .changeTextAnchor("{VAL}", "middle_bottom")
+export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("left")
-  .changeTextAnchor("{REF}", "middle_bottom")
+  .labelPort("left1", ["2", "neg"])
+  .labelPort("right1", ["1", "pos"])
+  .changeTextAnchor("{REF}", "middle_bottom", { x: 0, y: 0.78 })
+  .changeTextAnchor("{VAL}", "middle_top", { x: 0, y: -0.78 })
   .build()
