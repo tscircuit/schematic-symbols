a248824d4a8e0136af317cc2af0591a8a2fac53d
Devesh36 <142524747+Devesh36@users.noreply.github.com>
2026-02-15 22:23:18 +0530

    Update solderjumper2 SVG and TypeScript files for consistency and adjustments
    - Updated Inkscape version in solderjumper2.svg and adjusted window properties.
    - Modified path data for improved accuracy in solderjumper2.svg.
    - Changed import paths in solderjumper2_down.ts, solderjumper2_left.ts, solderjumper2_right.ts, and solderjumper2_up.ts for consistency.
    - Adjusted text anchor positions in solderjumper2_left.ts, solderjumper2_right.ts, and solderjumper2_up.ts to "middle_left" for better alignment.

--- symbols/solderjumper2_left.ts
+++ symbols/solderjumper2_left.ts
@@ -1,9 +1,9 @@
-import svgJson from "../assets/generated/solderjumper2.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
+import svgJson from "assets/generated/solderjumper2.json"
 
 export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("left")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
+  .changeTextAnchor("{REF}", "middle_left")
   .build()
