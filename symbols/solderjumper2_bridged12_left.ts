57324fe302c62ee229a3b4901d44e3a8901a6974
nailoo <rahulsinghnailoo@gmail.com>
2026-02-15 21:33:11 +0530

    fix ref text

--- symbols/solderjumper2_bridged12_left.ts
+++ symbols/solderjumper2_bridged12_left.ts
@@ -1,9 +1,16 @@
-import svgJson from "../assets/generated/solderjumper2_bridged12.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { rotateSymbol } from "drawing/rotateSymbol"
+import solderjumper2_bridged12_right from "./solderjumper2_bridged12_right"
 
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("left")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
-  .build()
+import type { TextPrimitive } from "drawing"
+
+const rotated = rotateSymbol(solderjumper2_bridged12_right, "left")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+
+ref.anchor = "middle_bottom"
+ref.y += 0.4
+ref.x = 0
+
+export default rotated
--- symbols/solderjumper2_bridged12_up.ts
+++ symbols/solderjumper2_bridged12_up.ts
@@ -1,9 +1,16 @@
-import svgJson from "../assets/generated/solderjumper2_bridged12.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { rotateSymbol } from "drawing/rotateSymbol"
+import solderjumper2_bridged12_right from "./solderjumper2_bridged12_right"
 
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("up")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_right")
-  .build()
+import type { TextPrimitive } from "drawing"
+
+const rotated = rotateSymbol(solderjumper2_bridged12_right, "up")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+
+ref.anchor = "middle_left"
+ref.y += 0
+ref.x = 0.2
+
+export default rotated
