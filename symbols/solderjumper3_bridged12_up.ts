07d4eb22655090258b931116d89d64ff19d37d8b
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2026-02-15 22:14:00 +0530

    fix: Adjust coordinates for the solderjumper3_bridged12 symbol (#394)
    * fix: Adjust coordinates for the solderjumper3_bridged12 symbol
    
    * fix ref text

--- symbols/solderjumper3_bridged12_up.ts
+++ symbols/solderjumper3_bridged12_up.ts
@@ -1,10 +1,16 @@
-import svgJson from "../assets/generated/solderjumper3_bridged12.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { rotateSymbol } from "drawing/rotateSymbol"
+import solderjumper3_bridged12_right from "./solderjumper3_bridged12_right"
 
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("up")
-  .labelPort("left1", ["1"])
-  .labelPort("bottom1", ["2"])
-  .labelPort("right1", ["3"])
-  .changeTextAnchor("{REF}", "middle_right")
-  .build()
+import type { TextPrimitive } from "drawing"
+
+const rotated = rotateSymbol(solderjumper3_bridged12_right, "up")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+
+ref.anchor = "middle_left"
+ref.y += 0.3
+ref.x = 0.2
+
+export default rotated
