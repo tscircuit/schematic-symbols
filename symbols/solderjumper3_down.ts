9298a7e8e47b2e35abf7cb0ffab929f80953b848
Devesh36 <142524747+Devesh36@users.noreply.github.com>
2026-02-19 18:08:13 +0530

    update the edges of solderjumper3

--- symbols/solderjumper3_down.ts
+++ symbols/solderjumper3_down.ts
@@ -1,10 +1,10 @@
-import svgJson from "../assets/generated/solderjumper3.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
+import svgJson from "assets/generated/solderjumper3.json"
 
 export default modifySymbol(svgJson)
   .rotateRightFacingSymbol("down")
-  .labelPort("left1", ["1"])
-  .labelPort("bottom1", ["2"])
+  .labelPort("bottom1", ["1"])
+  .labelPort("left1", ["2"])
   .labelPort("right1", ["3"])
   .changeTextAnchor("{REF}", "middle_left")
   .build()
