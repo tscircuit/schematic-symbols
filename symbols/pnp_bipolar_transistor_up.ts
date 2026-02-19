9ec5ca49804051ffd1b19092b31925d9739abbad
rushabhcodes <mail@rushabh.dev>
2026-02-20 00:17:38 +0530

    Fix text positions for REF and VAL labels in PNP bipolar transistor symbol

--- symbols/pnp_bipolar_transistor_up.ts
+++ symbols/pnp_bipolar_transistor_up.ts
@@ -1,35 +1,33 @@
 import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/pnp_bipolar_transistor.json"
+const { paths, bounds, refblocks, circles } = svgJson
 
 export default modifySymbol({
   primitives: [
-    ...Object.values(svgJson.paths),
-    ...Object.values(svgJson.circles),
+    ...Object.values(paths),
+    ...Object.values(circles),
     {
       type: "text",
       text: "{REF}",
-      x: -0.5,
+      x: 0.5,
       y: -0.3,
     },
     {
       type: "text",
       text: "{VAL}",
-      x: 0.5,
+      x: -0.5,
       y: -0.3,
     },
   ] as any,
   ports: [
-    { ...svgJson.refblocks.left1, labels: ["1", "collector"] },
-    { ...svgJson.refblocks.bottom1, labels: ["2", "base"] },
-    { ...svgJson.refblocks.right1, labels: ["3", "emitter"] },
+    { ...refblocks.left1, labels: ["1", "collector"] },
+    { ...refblocks.bottom1, labels: ["2", "base"] },
+    { ...refblocks.right1, labels: ["3", "emitter"] },
   ],
-  size: { width: svgJson.bounds.width, height: svgJson.bounds.height },
-  center: { x: svgJson.bounds.centerX, y: svgJson.bounds.centerY },
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
 })
   .rotateRightFacingSymbol("up")
-  .labelPort("bottom1", ["1"])
-  .labelPort("left1", ["2"])
-  .labelPort("right1", ["3"])
   .changeTextAnchor("{REF}", "middle_left")
   .changeTextAnchor("{VAL}", "middle_left")
   .build()
