f71ea693a687a266bc687d87688af9f6b3072eb4
rushabhcodes <mail@rushabh.dev>
2026-02-19 17:09:38 +0530

    fix: update text anchor positions for REF and VAL labels in rectifier diode symbol

--- symbols/rectifier_diode_vert.ts
+++ symbols/rectifier_diode_vert.ts
@@ -1,17 +1,29 @@
-import type { TextPrimitive } from "drawing"
-import { rotateSymbol } from "drawing/rotateSymbol"
-import rectifier_diode_horz from "./rectifier_diode_horz"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import svgJson from "assets/generated/rectifier_diode.json"
 
-const rotated = rotateSymbol(rectifier_diode_horz)
+const { paths, texts, bounds, refblocks, circles } = svgJson
 
-const ref = rotated.primitives.find(
-  (p) => p.type === "text" && p.text === "{REF}",
-)! as TextPrimitive
-const val = rotated.primitives.find(
-  (p) => p.type === "text" && p.text === "{VAL}",
-)! as TextPrimitive
-
-ref.anchor = "middle_left"
-val.anchor = "middle_right"
-
-export default rotated
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    { ...texts.top1, anchor: "middle_right" },
+    { ...texts.bottom1, anchor: "middle_right" },
+  ] as any,
+  ports: [
+    { ...refblocks.top1, labels: ["1"] },
+    { ...refblocks.bottom1, labels: ["2"] },
+    { ...refblocks.left1, labels: ["3"] },
+    { ...refblocks.right1, labels: ["4"] },
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .rotateRightFacingSymbol("down")
+  .labelPort("top1", ["1"])
+  .labelPort("bottom1", ["2"])
+  .labelPort("left1", ["3"])
+  .labelPort("right1", ["4"])
+  .changeTextAnchor("{REF}", "middle_right")
+  .changeTextAnchor("{VAL}", "middle_left")
+  .build()
