6553e2c854e205e43a6f020b861caebd9799d80d
rushabhcodes <mail@rushabh.dev>
2026-02-22 11:51:21 +0530

    Refactor rectifier diode symbols to update port labels and adjust SVG snapshots
    - Updated port labels for rectifier diode symbols (down, left, right, up) to maintain consistency after rotation.
    - Adjusted corresponding SVG snapshots to reflect the new port labels.

--- symbols/rectifier_diode_down.ts
+++ symbols/rectifier_diode_down.ts
@@ -11,19 +11,19 @@ export default modifySymbol({
     { ...texts.bottom1, x: 0.4, y: 0.35 },
   ] as any,
   ports: [
-    { ...refblocks.left1, labels: ["1"] },
-    { ...refblocks.top1, labels: ["2"] },
-    { ...refblocks.right1, labels: ["3"] },
-    { ...refblocks.bottom1, labels: ["4"] },
+    { ...refblocks.left1, labels: ["3"] },
+    { ...refblocks.top1, labels: ["4"] },
+    { ...refblocks.right1, labels: ["1"] },
+    { ...refblocks.bottom1, labels: ["2"] },
   ],
   size: { width: bounds.width, height: bounds.height },
   center: { x: bounds.centerX, y: bounds.centerY },
 })
   .rotateRightFacingSymbol("down")
-  .labelPort("bottom1", ["1"]) // becomes left after rotation
-  .labelPort("left1", ["2"]) // becomes up after rotation
-  .labelPort("top1", ["3"]) // becomes right after rotation
-  .labelPort("right1", ["4"]) // becomes down after rotation
+  .labelPort("bottom1", ["3"]) // becomes left after rotation
+  .labelPort("left1", ["4"]) // becomes up after rotation
+  .labelPort("top1", ["1"]) // becomes right after rotation
+  .labelPort("right1", ["2"]) // becomes down after rotation
   .changeTextAnchor("{REF}", "middle_left")
   .changeTextAnchor("{VAL}", "middle_left")
   .build()
