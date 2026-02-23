1926eaa66d09aed358cf74923665196e13012854
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2026-02-23 14:23:53 +0530

    simplify coordinate values for solderjumper2 and make symmetric symbol (#406)
    * refactor: simplify coordinate values for solderjumper2 schematic symbols and update their snapshots
    
    * update ref text

--- symbols/solderjumper2_left.ts
+++ symbols/solderjumper2_left.ts
@@ -1,9 +1,16 @@
-import svgJson from "../assets/generated/solderjumper2.json"
-import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
+import { rotateSymbol } from "drawing/rotateSymbol"
+import solderjumper2_right from "./solderjumper2_right"
 
-export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("left")
-  .labelPort("left1", ["1"])
-  .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_top")
-  .build()
+import type { TextPrimitive } from "drawing"
+
+const rotated = rotateSymbol(solderjumper2_right, "left")
+
+const ref = rotated.primitives.find(
+  (p) => p.type === "text" && p.text === "{REF}",
+)! as TextPrimitive
+
+ref.anchor = "middle_bottom"
+ref.y += 0.4
+ref.x = 0
+
+export default rotated
