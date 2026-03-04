e3ceeaf415ec999837d0d970bac28960ba3756d8
Shehab Ahmed <shehab924617940@gmail.com>
2026-03-04 22:29:16 +0200

    make mushroom symbol smooth and add 4 orintations

--- symbols/mushroom_head_normally_open_momentary_left.ts
+++ symbols/mushroom_head_normally_open_momentary_left.ts
@@ -2,9 +2,9 @@ import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/mushroom_head_normally_open_momentary.json"
 
 export default modifySymbol(svgJson)
-  .rotateRightFacingSymbol("down")
+  .rotateRightFacingSymbol("left")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
-  .changeTextAnchor("{REF}", "middle_left")
-  .changeTextAnchor("{VAL}", "middle_right", { x: -0.15, y: -0.17 })
+  .changeTextAnchor("{REF}", "middle_bottom", {x: 0, y: 0.7})
+  .changeTextAnchor("{VAL}", "middle_top", {x: -0.17, y: -0.55})
   .build()
