a700312a727d9ccba856ffc78a39918706ba8f71
krishnarajbaburadha@gmail.com <bkr.2007@yahoo.com>
2026-07-25 01:46:42 +0530

    fix(symbols): refine vertical zener diode text positioning and snapshot

--- symbols/zener_diode_vert.ts
+++ symbols/zener_diode_vert.ts
@@ -1,10 +1,35 @@
 import { modifySymbol } from "../drawing/modify-symbol/modify-symbol"
 import svgJson from "assets/generated/zener_diode.json"
 
-export default modifySymbol(svgJson)
+const { paths, bounds, refblocks, circles } = svgJson
+export default modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: 0.15,
+      y: -0.2294553499999995,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: -0.15,
+      y: -0.2294553499999995,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["1"] },
+    { ...refblocks.right1, labels: ["2"] },
+  ],
+  size: { width: bounds.width, height: bounds.height },
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
   .rotateRightFacingSymbol("up")
   .labelPort("left1", ["1"])
   .labelPort("right1", ["2"])
   .changeTextAnchor("{REF}", "middle_left")
   .changeTextAnchor("{VAL}", "middle_left")
   .build()
+
