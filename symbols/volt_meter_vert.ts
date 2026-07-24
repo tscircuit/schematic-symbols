2509b3d52bde7f6e82a0fe0d2879a7c30d63c470
gokul <gokulpandi0914@gmail.com>
2026-07-25 00:32:21 +0530

    fix: align volt_meter_vert labels on the right (#441)

--- symbols/volt_meter_vert.ts
+++ symbols/volt_meter_vert.ts
@@ -1,4 +1,36 @@
-import { rotateSymbol } from "drawing/rotateSymbol"
-import volt_meter_horz from "./volt_meter_horz"
+import svgJson from "assets/generated/volt_meter.json"
+import { modifySymbol } from "drawing/modify-symbol/modify-symbol"
 
-export default rotateSymbol(volt_meter_horz)
+const { paths, texts, bounds, circles, refblocks } = svgJson
+
+const symbol = modifySymbol({
+  primitives: [
+    ...Object.values(paths),
+    ...Object.values(circles),
+    {
+      type: "text",
+      text: "{REF}",
+      x: -0.2,
+      y: 0.35,
+    },
+    {
+      type: "text",
+      text: "{VAL}",
+      x: 0.2,
+      y: 0.35,
+    },
+  ] as any,
+  ports: [
+    { ...refblocks.left1, labels: ["left1"] },
+    { ...refblocks.right1, labels: ["right1"] },
+  ],
+  center: { x: bounds.centerX, y: bounds.centerY },
+})
+  .rotateRightFacingSymbol("down")
+  .labelPort("left1", ["1"])
+  .labelPort("right1", ["2"])
+  .changeTextAnchor("{REF}", "middle_left")
+  .changeTextAnchor("{VAL}", "middle_left")
+  .build()
+
+export default symbol
