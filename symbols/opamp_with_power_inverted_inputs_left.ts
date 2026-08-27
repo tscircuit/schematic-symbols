3b5b80abd30469f606a6bdad7ab1a63fe3c38201
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-08-27 10:51:09 +0200

    Add powered op-amp with inverted inputs

---
+++ symbols/opamp_with_power_inverted_inputs_left.ts
@@ -0,0 +1,6 @@
+import { flipSymbolOverXAxis, rotateSymbol } from "drawing/rotateSymbol"
+import opamp_with_power_inverted_inputs_right from "./opamp_with_power_inverted_inputs_right"
+
+export default flipSymbolOverXAxis(
+  rotateSymbol(opamp_with_power_inverted_inputs_right, "left"),
+)
