74d7009bcb802452159ade8f58cffd5aa67d51c5
Gokl-X-09 <gokulpandi0914@gmail.com>
2026-09-23 18:16:50 +0530

    up

--- symbols/varistor_vert.ts
+++ symbols/varistor_vert.ts
@@ -1,4 +1,23 @@
 import { rotateSymbol } from "drawing/rotateSymbol"
 import varistor_horz from "./varistor_horz"
 
-export default rotateSymbol(varistor_horz)
+const varistorVert = rotateSymbol(varistor_horz)
+const textPrimitives = varistorVert.primitives.filter(
+  (primitive) => primitive.type === "text",
+)
+const referenceText = textPrimitives.find(
+  (primitive) => primitive.text === "{REF}",
+)!
+const ratingText = textPrimitives.find(
+  (primitive) => primitive.text === "{VAL}",
+)!
+
+referenceText.x = 0.3
+referenceText.y = 0.15
+referenceText.anchor = "middle_left"
+
+ratingText.x = 0.3
+ratingText.y = -0.15
+ratingText.anchor = "middle_left"
+
+export default varistorVert
